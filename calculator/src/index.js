579663c3ce2d9e43536f792fc1bda4400ccc01d9
Alex Ng <alexkng97@gmail.com>
2020-10-14 22:05:58 +0100

    deleted old app js file

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Application from "./Application";
 
 ReactDOM.render(
   <React.StrictMode>
