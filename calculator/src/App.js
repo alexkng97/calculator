208385d03a3141fc31b6c48f4f15e01d6edff486
Alex Ng <alexkng97@gmail.com>
2020-10-14 11:32:37 +0100

    functionality complete

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -10,16 +10,39 @@ class App extends Component {
 
     this.state = {
       current: "",
-      previous: []
+      previous: [],
+      nextIsReset: false
     };
   }
 
   reset = () => {
-    this.setState({ result: "0" });
+    this.setState({ current: "", previous: [] });
   };
 
   addToCurrent = symbol => {
-    this.setState({ current: this.state.current + symbol });
+    if (["/", "-", "+", "*"].indexOf(symbol) > -1) {
+      let { previous } = this.state;
+      previous.push(this.state.current + symbol);
+      this.setState({ previous, nextIsReset: true });
+      console.log(this.state.previous);
+    } else {
+      if (
+        (this.state.current === "0" && symbol !== ".") ||
+        this.state.nextIsReset
+      ) {
+        this.setState({ current: symbol, nextIsReset: false });
+      } else {
+        this.setState({ current: this.state.current + symbol });
+      }
+    }
+  };
+
+  calculate = symbol => {
+    let { current, previous, nextIsReset } = this.state;
+    if (previous.length > 0) {
+      current = eval(String(previous[previous.length - 1] + current));
+      this.setState({ current, previous: [], nextIsReset: true });
+    }
   };
 
   render() {
@@ -29,7 +52,7 @@ class App extends Component {
       { symbol: "7", cols: 1, action: this.addToCurrent },
       { symbol: "8", cols: 1, action: this.addToCurrent },
       { symbol: "9", cols: 1, action: this.addToCurrent },
-      { symbol: "X", cols: 1, action: this.addToCurrent },
+      { symbol: "*", cols: 1, action: this.addToCurrent },
       { symbol: "4", cols: 1, action: this.addToCurrent },
       { symbol: "5", cols: 1, action: this.addToCurrent },
       { symbol: "6", cols: 1, action: this.addToCurrent },
@@ -37,11 +60,18 @@ class App extends Component {
       { symbol: "1", cols: 1, action: this.addToCurrent },
       { symbol: "2", cols: 1, action: this.addToCurrent },
       { symbol: "3", cols: 1, action: this.addToCurrent },
-      { symbol: "+", cols: 2, action: this.addToCurrent },
-      { symbol: "0", cols: 1, action: this.addToCurrent }
+      { symbol: "+", cols: 1, action: this.addToCurrent },
+      { symbol: "0", cols: 2, action: this.addToCurrent },
+      { symbol: ".", cols: 1, action: this.addToCurrent },
+      { symbol: "=", cols: 1, action: this.calculate }
     ];
     return (
       <div className="App">
+        {this.state.previous.length > 0 ? (
+          <div className="float-last">
+            {this.state.previous[this.state.previous.length - 1]}
+          </div>
+        ) : null}
         <input
           className="result"
           type="text"
